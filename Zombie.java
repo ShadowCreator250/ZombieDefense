e4a863351f421ca7bc1d6bdf118b1e2910a13508
ImSchaeterwaeter <74867704+ImSchaeterwaeter@users.noreply.github.com>
2020-11-24 20:24:59 +0100

    Update class Zombie
    -methods dropCurrency and attackGate added (not finished)
    -added constructor call as you said

--- Zombie.java
+++ Zombie.java
@@ -1,3 +1,5 @@
+import greenfoot.*;
+
 public class Zombie extends SmoothMover {
 	
 	public int strength;
@@ -5,13 +7,29 @@ public class Zombie extends SmoothMover {
 	public int speed;
 	
 	public Zombie() {
-		//TODO: image is temporarily
-		setImage("./ppl2.png");
+		this(10, 10, 10);		
 	}
 	
 	public Zombie(int strength, int resistance, int speed) {
+		//TODO: image is temporarily
+		setImage("./ppl2.png");
 		this.strength = strength;
 		this.resistance = resistance;
 		this.speed = speed;
+		dropCurrency(this);
+		attackGate();
 	} 
+	
+	public void dropCurrency(Zombie zombie) {
+		if (zombie.resistance <= 0) {
+			//TODO: Player gets money/gold + creating class money/gold
+			getWorld().removeObject(zombie);
+		}
+	}
+	
+	public void attackGate() {		
+		//if (getOneIntersectingObject(Gate.class)) {
+			//TODO: create class Gate, decrease the Gate´s health
+		//}
+	}
 }
