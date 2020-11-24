ac85e5f02f10d2cbee70b8a44816f7b9b439b030
ImSchaeterwaeter <74867704+ImSchaeterwaeter@users.noreply.github.com>
2020-11-24 19:21:12 +0100

    added new constructor in Zombie
    -new constructor with variables strength, resistance and speed

--- Zombie.java
+++ Zombie.java
@@ -1,6 +1,17 @@
 public class Zombie extends SmoothMover {
+	
+	public int strength;
+	public int resistance;
+	public int speed;
+	
 	public Zombie() {
 		//TODO: image is temporarily
 		setImage("./ppl2.png");
 	}
+	
+	public Zombie(int strength, int resistance, int speed) {
+		this.strength = strength;
+		this.resistance = resistance;
+		this.speed = speed;
+	} 
 }
