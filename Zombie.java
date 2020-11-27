2a365968f65cf73316167f8679ac738f7d813b83
ImSchaeterwaeter <74867704+ImSchaeterwaeter@users.noreply.github.com>
2020-11-27 19:12:45 +0100

    Update class Zombie
    -new variable health
    -resistance is directly involved in Tower classes
    -empty act-method added

--- Zombie.java
+++ Zombie.java
@@ -1,27 +1,30 @@
-import greenfoot.*;
 
 public class Zombie extends SmoothMover {
 	
 	public int strength;
-	public int resistance;
+	public double resistance; // should be from 0-1, example: 0.7 stands for 70% less damage to get
 	public int speed;
+	public double health;
 	
 	public Zombie() {
-		this(10, 10, 10);		
+		this(10, 0.1, 10, 100);		
 	}
 	
-	public Zombie(int strength, int resistance, int speed) {
+	public Zombie(int strength, double resistance, int speed, double health) {
 		//TODO: image is temporarily
 		setImage("./ppl2.png");
 		this.strength = strength;
 		this.resistance = resistance;
 		this.speed = speed;
-		dropCurrency(this);
-		attackGate();
+		this.health = health;
 	} 
 	
+	public void act() {
+		
+	}
+	
 	public void dropCurrency(Zombie zombie) {
-		if (zombie.resistance <= 0) {
+		if (zombie.health <= 0) {
 			//TODO: Player gets money/gold + creating class money/gold
 			getWorld().removeObject(zombie);
 		}
