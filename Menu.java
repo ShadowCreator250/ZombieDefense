4f8b7428a5513471c5573ce498845b6a1e9e371a
ShadowCreator250 <shadowcreator250+github@gmail.com>
2020-12-06 18:15:36 +0100

    add Coin Counter, prices, damages
    - add coin counter with initial value
    - add damage values and prices

--- Menu.java
+++ Menu.java
@@ -9,12 +9,17 @@ public class Menu extends Actor {
 
 	private List<MenuElement> menuElements = new ArrayList<>();
 	private final MenuElement mouseSelector = new MenuElement(this, calculateXOffset(1), 50, GameState.MouseState.NONE);
-	private final MenuElement archerTowerSelector = new MenuElement(this, calculateXOffset(2), 50, GameState.MouseState.PLACE_ARCHER_TOWER);
-	private final MenuElement bomberTowerSelector = new MenuElement(this, calculateXOffset(3), 50, GameState.MouseState.PLACE_BOMB_TOWER);
-	private final MenuElement sniperTowerSelector = new MenuElement(this, calculateXOffset(4), 50, GameState.MouseState.PLACE_SNIPER_TOWER);
+	private final MenuElement archerTowerSelector = new MenuElement(this, calculateXOffset(2), 50, GameState.MouseState.PLACE_ARCHER_TOWER,
+			ArcherTower.PRICE);
+	private final MenuElement bomberTowerSelector = new MenuElement(this, calculateXOffset(3), 50, GameState.MouseState.PLACE_BOMB_TOWER,
+			BombTower.PRICE);
+	private final MenuElement sniperTowerSelector = new MenuElement(this, calculateXOffset(4), 50, GameState.MouseState.PLACE_SNIPER_TOWER,
+			SniperTower.PRICE);
 	private final MenuElement deleteToolSelector = new MenuElement(this, calculateXOffset(1), 120, GameState.MouseState.DELETE_TOOL);
-	private final MenuElement mineFieldSelector = new MenuElement(this, calculateXOffset(2), 120, GameState.MouseState.PLACE_MINE_FIELD);
-	private final MenuElement slimeFieldSelector = new MenuElement(this, calculateXOffset(3), 120, GameState.MouseState.PLACE_SLIME_FIELD);
+	private final MenuElement mineFieldSelector = new MenuElement(this, calculateXOffset(2), 120, GameState.MouseState.PLACE_MINE_FIELD,
+			MineField.PRICE);
+	private final MenuElement slimeFieldSelector = new MenuElement(this, calculateXOffset(3), 120, GameState.MouseState.PLACE_SLIME_FIELD,
+			SlimeField.PRICE);
 
 	public Menu() {
 		setImage(BACKGROUND_IMAGE_NAME);
@@ -28,6 +33,7 @@ public class Menu extends Actor {
 			int xCoord = this.getX() - this.getImage().getWidth() / 2 + menuElement.getxOffset();
 			int yCoord = this.getY() - this.getImage().getHeight() / 2 + menuElement.getyOffset();
 			((GameWorld) getWorld()).addObject(menuElement, xCoord, yCoord);
+			menuElement.tryAddPriceTagToWorld();
 		}
 	}
 
@@ -43,6 +49,7 @@ public class Menu extends Actor {
 
 	public void removeYourself() {
 		for (MenuElement menuElement : menuElements) {
+			menuElement.removePriceTagIfExistent();
 			((GameWorld) getWorld()).removeObject(menuElement);
 		}
 		((GameWorld) getWorld()).removeObject(this);
