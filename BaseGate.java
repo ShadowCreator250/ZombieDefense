ee0031ec624afacee3d886f5fb89997b2adcfc4c
ImSchaeterwaeter <74867704+ImSchaeterwaeter@users.noreply.github.com>
2020-12-09 00:02:53 +0100

    fix

--- BaseGate.java
+++ BaseGate.java
@@ -1,45 +1,6 @@
 import greenfoot.Actor;
-import greenfoot.Greenfoot;
 
-/**
- * An object which blocks the way for the zombies to get into the survivors base. The player has to avoid that zombies attack it.
- */
 public class BaseGate extends Actor {
 	
-	private static final String GATE_IMAGE_NAME = "Gate.png";
 	
-	private double durability = 300;
-	
-	/**
-	 * Creates an base gate object and sets its image.
-	 */
-	public BaseGate() {
-		setImage(GATE_IMAGE_NAME);
-	}
-	
-	/**
-	 * @see checkForZombies()
-	 */
-	@Override
-	public void act() {
-		checkForZombies();
-	}
-	
-	/**
-	 * Looks, if zombies reach it.
-	 */
-	private void checkForZombies() {
-		for(Zombie zombie: getWorld().getObjects(Zombie.class)) {
-			Greenfoot.delay(50);
-			zombie.attackGate();
-		}
-	}
-	
-	public double getDurability() {
-		return durability;
-	}
-	
-	public void setDurability(double durability) {
-		this.durability = durability;
-	}
 }
